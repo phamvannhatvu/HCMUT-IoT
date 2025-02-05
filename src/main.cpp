08170a54816ca5f29967418c0e90faf68544a246
MSI\KAI <nhanksd85@gmail.com>
2025-02-05 13:30:39 +0700

    Lab2: Add Temperature and Humidity

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <DHT20.h>
+
 
 void TaskLEDControl(void *pvParameters) {
   pinMode(GPIO_NUM_48, OUTPUT); // Initialize LED pin
@@ -15,14 +17,33 @@ void TaskLEDControl(void *pvParameters) {
   }
 }
 
+void TaskTemperature_Humidity(void *pvParameters){
+  DHT20 dht20;
+  Wire.begin(GPIO_NUM_11, GPIO_NUM_12);
+  dht20.begin();
+  while(1){
+    dht20.read();
+    double temperature = dht20.getTemperature();
+    double humidity = dht20.getHumidity();
+
+    Serial.print("Temp: "); Serial.print(temperature); Serial.print(" *C ");
+    Serial.print(" Humidity: "); Serial.print(humidity); Serial.print(" %");
+    Serial.println();
+    
+    vTaskDelay(5000);
+  }
+
+}
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   xTaskCreate(TaskLEDControl, "LED Control", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskTemperature_Humidity, "LED Control", 2048, NULL, 2, NULL);
+  
 }
 
 void loop() {
-  // Serial.println("Hello Custom Board");
-  // delay(1000);
+  
 }
