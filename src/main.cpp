05ee75e0ec5e18eb8341ee1babac926c89b6a6de
phamvannhatvu <phamvannhatvu@gmail.com>
2025-04-14 22:53:25 +0700

    Add firmware version 1

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@ const std::array<IAPI_Implementation*, 3U> apis = {
 // Firmware title and version used to compare with remote version, to check if an update is needed.
 // Title needs to be the same and version needs to be different --> downgrading is possible
 constexpr char CURRENT_FIRMWARE_TITLE[] = "IoT_Lab3";
-constexpr char CURRENT_FIRMWARE_VERSION[] = "0";
+constexpr char CURRENT_FIRMWARE_VERSION[] = "1";
 // Maximum amount of retries we attempt to download each firmware chunck over MQTT
 constexpr uint8_t FIRMWARE_FAILURE_RETRIES = 12U;
 // Size of each firmware chunck downloaded over MQTT,
